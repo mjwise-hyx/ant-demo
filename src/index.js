f3d1479964ac43c769645ed663245511694ffa6e
heyongxian <1064042411@qq.com>
2018-10-23 16:32:13 +0800

    侧边栏实现

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SiderDemo  from './Navi/Navi';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SiderDemo  />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
