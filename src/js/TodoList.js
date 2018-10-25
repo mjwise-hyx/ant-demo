30b749afadf0a094d6111707f2fe5e43c5b3e9d0
heyongxian <1064042411@qq.com>
2018-10-25 20:19:25 +0800

    ajax-axios

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -1,8 +1,8 @@
 import React, { Component} from 'react';
-import 'antd/dist/antd.css';
-import {Input, Button, List} from 'antd';
+import TodoListUI from './TodoListUI';
 import store from '../store';
-
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction, initListAction } from '../store/actionCreator'
+import axios from 'axios'
 
 class TodoList extends Component {
 
@@ -10,37 +10,49 @@ class TodoList extends Component {
         super(props);
         this.state = store.getState();
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        this.handleItemDelete = this.handleItemDelete.bind(this);
+        //组件去订阅store
+        store.subscribe(this.handleStoreChange);
     }
 
     render(){
-        return (
-            <div style={{marginTop: '10px',marginLeft: '10px'}}>
-                <div>
-                    <Input 
-                        value={this.state.inputValue} 
-                        placeholder='todo' 
-                        style = {{width: '300px',marginRight: '10px'}}
-                        onChange = {this.handleInputChange}
-                        />
-                    <Button type = "primary">提交</Button>
-                </div>
-                <List
-                    style={{marginTop: '10px', width:'300px'}}
-                    bordered
-                    dataSource={this.state.list}
-                    renderItem={item => (<List.Item>{item}</List.Item>)}
-                    />
-            </div>
-        );
+        return <TodoListUI 
+            inputValue = {this.state.inputValue}
+            list = {this.state.list}
+            handleInputChange = {this.handleInputChange}
+            handleBtnClick = {this.handleBtnClick}
+            handleItemDelete = {this.handleItemDelete}
+         />
+    }
+
+    componentDidMount(){
+        axios.get('/api/todolist').then((res)=>{
+            const data = res.data;
+            const action = initListAction(data);
+            store.dispatch(action);
+        })
+    }
+
+    handleStoreChange(){
+        this.setState(store.getState());
     }
 
     handleInputChange(e){
-        const action = {
-            type: 'change_input_value',
-            value: e.target.value
-        }
-        store.dispach(action);
-    } 
+        const action = getInputChangeAction(e.target.value);
+        store.dispatch(action);
+    }
+
+    handleBtnClick(){
+        const action = getAddItemAction();
+        store.dispatch(action);
+    }
+
+    handleItemDelete(index){
+        const action = getDeleteItemAction(index);
+        store.dispatch(action);
+    }
 }
 
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,36 @@
+import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM, INIT_LIST_ACTION} from './actionTypes'
+
 const defaultState = {
-    inputValue: 'qqq',
-    list: ['a','b']
+    inputValue: '',
+    list: []
 }
 
+//reducer 可以接受state,但是绝不能改变state
+//接受action,action.type判断接收类型
+//纯函数，给定固定的输入，就一定有固定的输出，而且不会有任何的副作用
 export default (state = defaultState,action) =>{
+
+    if (action.type === CHANGE_INPUT_VALUE){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.inputValue = action.value;
+        return newState;
+    }
+    if (action.type === INIT_LIST_ACTION) {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list = action.data;
+        return newState;
+    }
+    if (action.type === ADD_TODO_ITEM){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue = '';
+        return newState;
+    }
+    if(action.type === DELETE_TODO_ITEM){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.splice(action.index,1);
+        return newState;
+    }
+
     return state;
 }
