3856c592b11eedb91f378f2fc1b61a5c9a65d49a
heyongxian <1064042411@qq.com>
2018-10-25 15:54:50 +0800

    动画效果CSSTran

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component ,Fragment} from 'react';
-
+import { CSSTransition } from 'react-transition-group';
 import './css/App.css';
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            show: true
+            list: []
         }
         this.handleToggole = this.handleToggole.bind(this);
     }
@@ -16,14 +16,39 @@ class App extends Component {
         return (
             //Fragment占位
             <Fragment>
-                <div className = {this.state.show ? 'show' : 'hide'}>
-                    hello, dell lee
-                </div>
+                <CSSTransition
+                    in = {this.state.show}
+                    timeout = {500}
+                    classNames = 'fade'
+                    unmountOnExit
+                    onEntered={(el) => {el.style.color='blue'}}
+                    appear = {true}
+                >
+                    <div>
+                        hello, dell lee
+                    </div>
+                </CSSTransition>
+
+                {
+                    this.state.list.map((item) => {
+                        return (
+                            <div>{item}</div>
+                        )
+                    })
+                }
                 <button 
                     onClick={this.handleToggole}
                 >toggle</button>
             </Fragment>
         );
+    }
+
+    handleAddItem(){
+        this.setState((prevState)=>{
+            return {
+                list: [...prevState.list,'item']
+            }
+        })
     }
 
     handleToggole(){
